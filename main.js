ef3d5cbd1402759462ccbd70637bf992d69ed8d2
J Elie <105410303+J-Elie@users.noreplay.github.com>
2023-11-02 23:27:34 -0400

    add register js

---
+++ main.js
@@ -0,0 +1,37 @@
+let registerForm = document.getElementById("registerForm")
+registerForm.addEventListener('submit', register)
+
+function register(e) {
+    e.preventDefault();
+    let fname = document.getElementById("fname").value
+    let lname = document.getElementById("lname").value
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    let confirmPassword = document.getElementById("confirmPassword").value
+    
+    let rightRegister = document.getElementById("rightRegister")
+    rightRegister.innerHTML = 
+    `<div><h2>Welcome to EchoNotes
+     ${fname} ${lname} ${email}
+     </h2></div>`
+}
+
+function validatePassword() {
+ 
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    if (password != confirmPassword) {
+        document.getElementById('passwordAlert').style.color = 'red';
+        document.getElementById('passwordAlert').innerHTML
+            = '☒ <br>Password does not match';
+        document.getElementById('submitButton').disabled = true;
+        document.getElementById('submitButton').style.opacity = (0.6);
+    } else {
+        document.getElementById('passwordAlert').style.color = 'green';
+        document.getElementById('passwordAlert').innerHTML =
+            '🗹';
+        document.getElementById('submitButton').disabled = false;
+        document.getElementById('submitButton').style.opacity = (1);
+    }
+}
+
