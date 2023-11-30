6153f2b3318202433dfb1e3ea63e4fb4fefd85d5
J Elie <105410303+J-Elie@users.noreplay.github.com>
2023-11-30 00:23:53 -0500

    update form validation

--- public/Scripts/user.js
+++ public/Scripts/user.js
@@ -18,12 +18,16 @@ TOC
 2. Input Validation
 ***************/
 
-/*----------------------------------------------
+
+/*-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 0. Register Form and Register Validation
-------------------------------------------------*/
+-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#*/
 let registerForm = document.getElementById("registerForm");
 registerForm?.addEventListener("submit", register);
-// creates a user object if calling validateRegisterForm() returns true, else prints error to the console.
+/**
+ * register creates a user object if calling validateRegisterForm() returns true, else prints error to the console.
+ * @param {*} e preventDefault()
+ */
 function register(e) {
   e.preventDefault();
   if (validateRegisterForm()) {
@@ -46,31 +50,249 @@ function register(e) {
     console.log("error invalid form");
   }
 }
-// calls all input validation functions for the register form
+/**
+ * validateRegisterForm
+ * @returns true if all register form fields are properly formatted
+ */
 function validateRegisterForm() {
   var valid = false;
   if (
-    validateFname() &&
-    validateLname() &&
-    validateEmail() &&
-    validatePassword() &&
-    validateConfirmPassword()
+    validateFnameRegister() &&
+    validateLnameRegister() &&
+    validateEmailRegister() &&
+    validatePasswordRegister() &&
+    validateConfirmPasswordRegister()
   ) {
     return true;
   } else {
-    validateFname();
-    validateLname();
-    validateEmail();
-    validatePassword();
-    validateConfirmPassword();
+    validateFnameRegister();
+    validateLnameRegister();
+    validateEmailRegister();
+    validatePasswordRegister();
+    validateConfirmPasswordRegister();
   }
 }
-/*----------------------------------------------
+/**
+ * validateFnameRegister
+ * @returns true if first name field is valid
+ */
+function validateFnameRegister() {
+  var fname = document.getElementById("fname").value;
+  var successIcon = document.getElementById("fnameSuccess");
+  var errorIcon = document.getElementById("fnameError");
+  var errorMSG = document.getElementById("fnameErrorMSG");
+  var label = document
+    .getElementById("fnameLabel")
+    .textContent.replace(":", "");
+  var length = 30;
+  var fnameRegex = /^[a-zA-Z]*$/g;
+  switch (true) {
+    case fname === "":
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br>${label} can not be blank`
+      );
+      break;
+    case fname.length > length:
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br>${label} can not be longer than ${length} characters`
+      );
+      break;
+    case !fnameRegex.test(fname):
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br> Invalid character in ${label}`
+      );
+      break;
+    default:
+      validateInput(successIcon, errorIcon, errorMSG, "");
+      return true;
+  }
+}
+/**
+ * validateLnameRegister
+ * @returns true if last name field is valid
+ */
+function validateLnameRegister() {
+  var lname = document.getElementById("lname").value;
+  var successIcon = document.getElementById("lnameSuccess");
+  var errorIcon = document.getElementById("lnameError");
+  var errorMSG = document.getElementById("lnameErrorMSG");
+  var label = document
+    .getElementById("lnameLabel")
+    .textContent.replace(":", "");
+  var length = 30;
+  var lnameRegex = /^[a-zA-Z]*$/g;
+  switch (true) {
+    case lname === "":
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br>${label} can not be blank`
+      );
+      break;
+    case lname.length > length:
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br>${label}can not be longer than ${length} characters`
+      );
+      break;
+    case !lnameRegex.test(lname):
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br> Invalid character in ${label}`
+      );
+      break;
+    default:
+      validateInput(successIcon, errorIcon, errorMSG, "");
+      return true;
+  }
+}
+/**
+ * validateEmailRegister
+ * @returns true if email field is valid
+ */
+function validateEmailRegister() {
+  var email = document.getElementById("email").value;
+  var successIcon = document.getElementById("emailSuccess");
+  var errorIcon = document.getElementById("emailError");
+  var errorMSG = document.getElementById("emailErrorMSG");
+  var label = document
+    .getElementById("emailLabel")
+    .textContent.replace(":", "");
+  var length = 30;
+  var emailRegex = /^\S+@\S+$/; //very simple email regex for testing purposes
+  switch (true) {
+    case email === "":
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br>${label} can not be blank`
+      );
+      break;
+    case email.length > length:
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br>${label}can not be longer than ${length} characters`
+      );
+      break;
+    case !emailRegex.test(email):
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br> Invalid ${label} format, standard format is <br>user@example.com`
+      );
+      break;
+    // case email is in use:
+    //   validateInput(errorIcon, successIcon, errorMSG, `<br>The ${label} you entered is already in use`)
+    //   break;
+    default:
+      validateInput(successIcon, errorIcon, errorMSG, "");
+      return true;
+  }
+}
+/**
+ * validatePasswordRegister
+ * @returns true if the password field is valid
+ */
+function validatePasswordRegister() {
+  var password = document.getElementById("password").value;
+  var successIcon = document.getElementById("passwordSuccess");
+  var errorIcon = document.getElementById("passwordError");
+  var errorMSG = document.getElementById("passwordErrorMSG");
+  var label = document
+    .getElementById("passwordLabel")
+    .textContent.replace(":", "");
+  var minLength = 3;
+  var passwordRegex = /^[a-zA-Z0-9!@#$%^&*]{3,}$/g;
+  switch (true) {
+    case password === "" || password.length < minLength:
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br>${label} must contain at least 3 characters`
+      );
+      break;
+    case !passwordRegex.test(password):
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br> Invalid ${label} character`
+      );
+      break;
+    default:
+      validateInput(successIcon, errorIcon, errorMSG, "");
+      var confirmPassword = document.getElementById("confirmPassword").value;
+      if (confirmPassword != "") {
+        validateConfirmPasswordRegister();
+      }
+      return true;
+  }
+}
+/**
+ * validateConfirmPasswordRegister
+ * @returns true if the confirm password field is valid
+ */
+function validateConfirmPasswordRegister() {
+  var confirmPassword = document.getElementById("confirmPassword").value;
+  var password = document.getElementById("password").value;
+  var successIcon = document.getElementById("confirmPasswordSuccess");
+  var errorIcon = document.getElementById("confirmPasswordError");
+  var errorMSG = document.getElementById("confirmPasswordErrorMSG");
+  var label = document
+    .getElementById("confirmPasswordLabel")
+    .textContent.replace(":", "");
+  switch (true) {
+    case password != confirmPassword:
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br>${label} does not match`
+      );
+      break;
+    case confirmPassword == "":
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br>${label} can not be blank`
+      );
+      break;
+    default:
+      validateInput(successIcon, errorIcon, errorMSG, "");
+      return true;
+  }
+}
+
+
+/*-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 1. Login Form Login Validation
-------------------------------------------------*/
+-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#*/
 let loginForm = document.getElementById("loginForm");
 loginForm?.addEventListener("submit", login);
-
+/**
+ * login creates a user object if calling validateLoginForm() returns true, else prints error to the console.
+ * @param {*} e preventDefault()
+ */
 function login(e) {
   e.preventDefault();
   if (validateLoginForm()) {
@@ -84,7 +306,10 @@ function login(e) {
     console.log("error invalid form");
   }
 }
-// calls all input validation functions for the login form
+/**
+ * validateLoginForm
+ * @returns true if all login form fields are properly formatted
+ */
 function validateLoginForm() {
   var valid = false;
   if (validateEmailLogin() && validatePasswordLogin()) {
@@ -94,101 +319,11 @@ function validateLoginForm() {
     validatePasswordLogin();
   }
 }
-/*----------------------------------------------
-2. Input Validation
-------------------------------------------------*/
-//function to validate the first name field on key up
-function validateFname() {
-  var fname = document.getElementById("fname").value;
-  var successIcon = document.getElementById("fnameSuccess");
-  var errorIcon = document.getElementById("fnameError");
-  var errorMSG = document.getElementById("fnameErrorMSG");
-  var label = document
-    .getElementById("fnameLabel")
-    .textContent.replace(":", "");
-  var length = 30;
-  var fnameRegex = /^[a-zA-Z]*$/g;
-  if (fname === "") {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br>The ${label} field can not be blank`;
-  } else if (fname.length > length) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br>${label} field can not be longer than ${length} characters`;
-  } else if (!fnameRegex.test(fname)) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br> Invalid character in the ${label} field`;
-  } else {
-    successIcon.classList.remove("hidden");
-    errorIcon.classList.add("hidden");
-    errorMSG.innerHTML = "";
-    return true;
-  }
-}
-//function to validate the last name field on key up
-function validateLname() {
-  var lname = document.getElementById("lname").value;
-  var successIcon = document.getElementById("lnameSuccess");
-  var errorIcon = document.getElementById("lnameError");
-  var errorMSG = document.getElementById("lnameErrorMSG");
-  var label = document
-    .getElementById("lnameLabel")
-    .textContent.replace(":", "");
-  var length = 30;
-  var lnameRegex = /^[a-zA-Z]*$/g;
-  if (lname === "") {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br>The ${label} field can not be blank`;
-  } else if (lname.length > length) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br>${label} field can not be longer than ${length} characters`;
-  } else if (!lnameRegex.test(lname)) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br> Invalid character in the ${label} field`;
-  } else {
-    successIcon.classList.remove("hidden");
-    errorIcon.classList.add("hidden");
-    errorMSG.innerHTML = "";
-    return true;
-  }
-}
-//function to validate the email address field on key up
-function validateEmail() {
-  var email = document.getElementById("email").value;
-  var successIcon = document.getElementById("emailSuccess");
-  var errorIcon = document.getElementById("emailError");
-  var errorMSG = document.getElementById("emailErrorMSG");
-  var label = document
-    .getElementById("emailLabel")
-    .textContent.replace(":", "");
-  var length = 30;
-  var emailRegex = /^\S+@\S+$/; //very simple email regex for testing purposes
-  if (email === "") {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br>${label} can not be blank`;
-  } else if (email.length > length) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br>${label} field can not be longer than ${length} characters`;
-  } else if (!emailRegex.test(email)) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br> Invalid ${label} format, standard format is <br>user@example.com`;
-  } else {
-    successIcon.classList.remove("hidden");
-    errorIcon.classList.add("hidden");
-    errorMSG.innerHTML = "";
-    return true;
-  }
-}
-//function to validate the password field on key up
-function validatePassword() {
+/**
+ * validatePasswordLogin
+ * @returns true if login password properly formatted
+ */
+function validatePasswordLogin() {
   var password = document.getElementById("password").value;
   var successIcon = document.getElementById("passwordSuccess");
   var errorIcon = document.getElementById("passwordError");
@@ -198,102 +333,64 @@ function validatePassword() {
     .textContent.replace(":", "");
   var minLength = 3;
   var passwordRegex = /^[a-zA-Z0-9!@#$%^&*]{3,}$/g;
-  if (password === "" || password.length < minLength) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br>${label} must contain at least 3 characters`;
-  } else if (!passwordRegex.test(password)) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br> Invalid ${label} format, standard format is <br>user@example.com`;
-  } else {
-    successIcon.classList.remove("hidden");
-    errorIcon.classList.add("hidden");
-    errorMSG.innerHTML = "";
-    var confirmPassword = document.getElementById("confirmPassword").value;
-    if (confirmPassword != "") {
-      validateConfirmPassword();
-    }
-    return true;
-  }
-}
-//function to validate the confirm password field on key up
-function validateConfirmPassword() {
-  var confirmPassword = document.getElementById("confirmPassword").value;
-  var password = document.getElementById("password").value;
-  var successIcon = document.getElementById("confirmPasswordSuccess");
-  var errorIcon = document.getElementById("confirmPasswordError");
-  var errorMSG = document.getElementById("confirmPasswordErrorMSG");
-  var label = document
-    .getElementById("confirmPasswordLabel")
-    .textContent.replace(":", "");
-  if (password != confirmPassword) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br>${label} does not match`;
-  } else if ((confirmPassword = "")) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br>${label} can not be blank`;
-  } else {
-    successIcon.classList.remove("hidden");
-    errorIcon.classList.add("hidden");
-    errorMSG.innerHTML = "";
-    return true;
-  }
-}
-//there will be a better solution in the future, but for now extra functions for login
-//function to validate the password field on key up
-function validatePasswordLogin() {
-  var password = document.getElementById("password").value;
-  var successIcon = document.getElementById("passwordSuccess");
-  var errorIcon = document.getElementById("passwordError");
-  var errorMSG = document.getElementById("passwordErrorMSG");
-  var label = document.getElementById("passwordLabel").textContent.replace(":", "");
-  var minLength = 3;
-  var passwordRegex = /^[a-zA-Z0-9!@#$%^&*]{3,}$/g;
-  if (password === "" || password.length < minLength) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br>${label} must contain at least 3 characters`;
-  } else if (!passwordRegex.test(password)) {
-    successIcon.classList.add("hidden");
-    errorIcon.classList.remove("hidden");
-    errorMSG.innerHTML = `<br> Invalid ${label} format, standard format is <br>user@example.com`;
-  } else {
-    successIcon.classList.remove("hidden");
-    errorIcon.classList.add("hidden");
-    errorMSG.innerHTML = "";
-    return true;
-  }
-}
-//function to validate the email address field on key up
-function validateEmailLogin() {
-    var email = document.getElementById("email").value;
-    var successIcon = document.getElementById("emailSuccess");
-    var errorIcon = document.getElementById("emailError");
-    var errorMSG = document.getElementById("emailErrorMSG");
-    var label = document
-      .getElementById("emailLabel")
-      .textContent.replace(":", "");
-    var length = 30;
-    var emailRegex = /^\S+@\S+$/; //very simple email regex for testing purposes
-    if (email === "") {
-      successIcon.classList.add("hidden");
-      errorIcon.classList.remove("hidden");
-      errorMSG.innerHTML = `<br>${label} can not be blank`;
-    } else if (email.length > length) {
-      successIcon.classList.add("hidden");
-      errorIcon.classList.remove("hidden");
-      errorMSG.innerHTML = `<br>${label} field can not be longer than ${length} characters`;
-    } else if (!emailRegex.test(email)) {
-      successIcon.classList.add("hidden");
-      errorIcon.classList.remove("hidden");
-      errorMSG.innerHTML = `<br> Invalid ${label} format, standard format is <br>user@example.com`;
-    } else {
-      successIcon.classList.remove("hidden");
-      errorIcon.classList.add("hidden");
-      errorMSG.innerHTML = "";
+  switch (true) {
+    case password === "" ||
+      password.length < minLength ||
+      !passwordRegex.test(password):
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br>Incorrect or invalid${label}`
+      );
+      break;
+    default:
+      validateInput(successIcon, errorIcon, errorMSG, "");
       return true;
-    }
+  }
+}
+/**
+ * validateEmailLogin
+ * @returns true if login email properly formatted
+ */
+function validateEmailLogin() {
+  var email = document.getElementById("email").value;
+  var successIcon = document.getElementById("emailSuccess");
+  var errorIcon = document.getElementById("emailError");
+  var errorMSG = document.getElementById("emailErrorMSG");
+  var label = document
+    .getElementById("emailLabel")
+    .textContent.replace(":", "");
+  var length = 30;
+  var emailRegex = /^\S+@\S+$/; //very simple email regex for testing purposes
+  switch (true) {
+    case email === "" || email.length > length || !emailRegex.test(email):
+      validateInput(
+        errorIcon,
+        successIcon,
+        errorMSG,
+        `<br>Incorrect or invalid${label}`
+      );
+      break;
+    default:
+      validateInput(successIcon, errorIcon, errorMSG, "");
+      return true;
+  }
+}
+
+
+/*-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+2. Input Validation
+-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#*/
+/**
+ * validateInput
+ * @param {*} show - removes the class of hidden to show the element passed
+ * @param {*} hide - add the class of hidden to hide the element passed
+ * @param {*} msgContainer - element that will hold the msg
+ * @param {*} msg - message to be placed in the msgContainer element
+ */
+function validateInput(show, hide, msgContainer, msg) {
+  hide.classList.add("hidden");
+  show.classList.remove("hidden");
+  msgContainer.innerHTML = msg;
 }
